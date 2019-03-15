6f02ce74a85feb72dbea4290afa34465240c575b
Maximilian Wong <maximilian520@163.com>
2019-03-15 14:10:58 +0800

    add rander util

--- go.mod
+++ go.mod
@@ -3,12 +3,10 @@ module github.com/MaxTech/commons
 go 1.12
 
 require (
+	github.com/gin-contrib/multitemplate v0.0.0-20190301062633-f9896279eead // indirect
 	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
-	github.com/gin-gonic/gin v1.3.0 // indirect
 	github.com/go-redis/redis v6.15.2+incompatible // indirect
 	github.com/golang/protobuf v1.3.0 // indirect
 	github.com/mattn/go-isatty v0.0.6 // indirect
 	github.com/ugorji/go/codec v0.0.0-20190309163734-c4a1c341dc93 // indirect
-	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
-	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
---
+++ template_utils.go
@@ -0,0 +1,55 @@
+package commons
+
+import (
+    "fmt"
+    "github.com/gin-contrib/multitemplate"
+    "path/filepath"
+    "strings"
+)
+
+func CreateCommonRender(templatesDir string) multitemplate.Renderer {
+    renderer := multitemplate.NewRenderer()
+
+    layouts, err := filepath.Glob(fmt.Sprintf("%s/%s", templatesDir, "layouts/*.html"))
+    if err != nil {
+        panic(err.Error())
+    }
+
+    contents, err := filepath.Glob(fmt.Sprintf("%s/%s", templatesDir, "contents/**/*.html"))
+    if err != nil {
+        panic(err.Error())
+    }
+
+    for _, content := range contents {
+        dirList := strings.Split(content, "/")
+        dirName := dirList[len(dirList)-2]
+
+        includes, err := filepath.Glob(fmt.Sprintf("%s/%s/%s/%s", templatesDir, "includes", dirName, "*.html"))
+        if err != nil {
+            panic(err.Error())
+        }
+
+        layoutCopy := make([]string, len(layouts))
+        copy(layoutCopy, layouts)
+
+        includeCopy := make([]string, len(includes))
+        copy(includeCopy, includes)
+
+        files := append(layoutCopy, includeCopy...)
+        files = append(files, content)
+
+        renderer.AddFromFiles(dirName, files...)
+    }
+
+    defaults, err := filepath.Glob(fmt.Sprintf("%s/%s", templatesDir, "default/*.html"))
+    if err != nil {
+        panic(err.Error())
+    }
+
+    for _, page := range defaults {
+        renderer.AddFromFiles(filepath.Base(page), page)
+    }
+
+    return renderer
+}
+
